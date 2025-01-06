9ba07cc40dd0ee4359a860aea877a6e1c45755c2
Đỗ Quang Huy <149219260+Huy132005@users.noreply.github.com>
2025-01-06 22:08:38 +0700

    add

--- MVC/views/admin/users/create.php
+++ MVC/views/admin/users/create.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['success'])) {
     </div>
     <div class="mb-3 mt-3">
         <label for="email" class="form-label">Email:</label>
-        <input type="email" class="form-control" id="email" name="email">
+        <input type="email" class="form-control" id="email" name="email"value="<?= $_SESSION['data']['email'] ?? null ?>">
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Password:</label>
